305c90b50e7bd0d0992e1aa529564a40ccba988e
hamisirizwan <hamisirizwan5@gmail.com>
2023-02-25 19:32:38 +0300

    update user done

--- server/API/Controllers/userControllers/update.js
+++ server/API/Controllers/userControllers/update.js
@@ -1,8 +1,57 @@
 const prisma = require("../../DB/prisma");
+const validator = require("../../Utilities/validator");
 
-const UpdateUser = (req, res) => {
+const UpdateUser = async (req, res) => {
+  const user_id = parseInt(req.params.id);
+
+  const data = req.body;
   try {
-    res.send("UpdateUser");
+    //check for no data
+    if (Object.keys(data).length === 0) {
+      throw {
+        custom: true,
+        message: "no data to update",
+      };
+    }
+
+    //if email provided
+    if (data.email) {
+      if (!validator.email(data.email)) {
+        res.status(200).json("invalid email");
+      }
+    }
+
+    //if phone provided
+    if (data.phone) {
+      if (!validator.phone(data.phone)) {
+        res.status(200).json("invalid phone number");
+      }
+    }
+
+    //find user
+    const existingUser = await prisma.users.findUnique({
+      where: {
+        id: user_id,
+      },
+    });
+    if (!existingUser) {
+      res.status(400).json("user not found");
+    }
+
+    const updatedUser = await prisma.users.update({
+      where: {
+        id: user_id,
+      },
+      data: {
+        name: data.name || existingUser.name,
+        email: data.email || existingUser.email,
+        phone: data.phone || existingUser.phone,
+      },
+    });
+    res.status(200).json({
+      message: "Details updated successfully.",
+      data: updatedUser,
+    });
   } catch (error) {
     console.log(error);
     res.status(400).json(error.message);
