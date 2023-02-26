e5a2f38c40d70a4c55797edeec26f3b26b40bdb2
hamisirizwan <hamisirizwan5@gmail.com>
2023-02-26 12:55:59 +0300

    working on frontend user management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:1234/api/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import ticketImage from "../../assets/icons/ticketing.png";
+import emailValidator from "email-validator";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function SignIn() {
-  const handleSubmit = (e) => {
+  const [inputData, setInputData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+    // console.log(details);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("login logic goes here");
+
+    if (!inputData.email || !inputData.password) {
+      return toast.error("all fields required");
+    }
+    if (!emailValidator.validate(inputData.email)) {
+      return toast.error("invalid email");
+    }
+
+    await axios
+      .post("/user/login", inputData)
+      .then((res) => {
+        console.log(res);
+        toast.success();
+        //handle user auth redux
+      })
+      .catch((err) => {
+        toast.error(err.response.data);
+        console.log(err.response.data);
+      });
   };
   return (
     <section className="grid md:grid-cols-2  h-screen">
@@ -16,19 +49,32 @@ function SignIn() {
           <h1 className="text-2xl font-medium text-center mt-4">Log In</h1>
           <form className="my-4 mx-3" onSubmit={handleSubmit}>
             <div className="py-4">
+              {!inputData.email && (
+                <h1 className="text-xs text-red-500">required</h1>
+              )}
               <input
                 type="email"
                 name="email"
                 placeholder="enter email"
                 className=" block w-full border-b-2 focus:border-primary focus:outline-none "
+                value={inputData.email}
+                // required
+                onChange={handleChange}
               />
             </div>
             <div className="py-4">
+              {!inputData.password && (
+                <h1 className="text-xs text-red-500">required</h1>
+              )}
+
               <input
                 type="password"
                 name="password"
                 placeholder="enter password"
-                className="block w-full border-b-2 focus:border-primary focus:outline-none "
+                className="block w-full border-b-2 focus:border-primary focus:outline-none"
+                value={inputData.password}
+                // required
+                onChange={handleChange}
               />
             </div>
             <button
@@ -40,6 +86,7 @@ function SignIn() {
           </form>
         </div>
       </div>
+      <ToastContainer autoClose={2000} />
     </section>
   );
 }
