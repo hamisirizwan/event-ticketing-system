885a5b627323bfca6a2514960bebdf02865e4dbf
hamisirizwan <hamisirizwan5@gmail.com>
2023-03-01 16:06:12 +0300

    added events listing

---
+++ client/src/components/layouts/reusables/EventCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function EventCard(props) {
+  return (
+    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+      <a className="block relative h-48 rounded overflow-hidden">
+        <img
+          alt="event image"
+          className="object-cover object-center w-full h-full block"
+          src={props.imageurl}
+        />
+      </a>
+      <div className="mt-4">
+        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+          {props.title}
+        </h3>
+        <h2 className="text-gray-900 title-font text-lg font-medium">
+          {props.description}
+        </h2>
+        <p className="mt-1">{props.price} KSH</p>
+      </div>
+      <div className="mt-4">
+        <button
+          type="button"
+          className="py-2 px-4  bg-primary hover:bg-secondary focus:ring-primary focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+        >
+          Book Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
--- client/src/components/sections/Hero.jsx
+++ client/src/components/sections/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import HeroImg from "../../assets/hero1.png";
 import HeroImg from "../../assets/ticketing.svg";
+
 function HeroSection() {
   return (
     <div>
-      <div className="bg-white dark:bg-gray-800 overflow-hidden relative lg:flex lg:items-center">
+      <div className="bg-white dark:bg-gray-800 overflow-hidden  md:pt-10 relative lg:flex lg:items-center">
         <div className="w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
           <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
             <span className="block">
@@ -19,12 +21,14 @@ function HeroSection() {
           </p>
           <div className="lg:mt-0 lg:flex-shrink-0">
             <div className="mt-12 inline-flex rounded-md shadow">
-              <button
-                type="button"
-                className="py-2 px-4  bg-primary hover:bg-secondary focus:ring-primary focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-              >
-                Go To Console
-              </button>
+              <Link to="/sign-in">
+                <button
+                  type="button"
+                  className="py-2 px-4  bg-primary hover:bg-secondary focus:ring-primary focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  Go To Console
+                </button>
+              </Link>
             </div>
           </div>
         </div>
