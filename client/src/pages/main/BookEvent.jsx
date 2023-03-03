5b17e26690be6bf5997902e88090ea69dff03cf7
hamisirizwan <hamisirizwan5@gmail.com>
2023-03-03 11:12:36 +0300

    api calls error handling

--- client/src/pages/main/BookEvent.jsx
+++ client/src/pages/main/BookEvent.jsx
@@ -11,6 +11,7 @@ import { useParams } from "react-router-dom";
 function BookEvent() {
   const [event, setEvent] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [inputData, setInputData] = useState({
     name: "",
     phone: "",
@@ -30,6 +31,7 @@ function BookEvent() {
       .catch((err) => {
         console.log(err);
         setLoading(false);
+        setError(true);
       });
   };
 
@@ -46,8 +48,22 @@ function BookEvent() {
     if (!inputData.name || !inputData.phone) {
       return toast.error("all fields required");
     }
-    let price = event.price;
-    alert(price);
+    if (inputData.phone.length !== 10) {
+      return toast.error("Invalid phone number");
+    }
+    if (
+      inputData.phone.substring(0, 2) !== "07" &&
+      inputData.phone.substring(0, 2) !== "01"
+    ) {
+      return toast.error("Invalid phone number");
+    }
+
+    //send request
+    console.log({
+      name: inputData.name,
+      phone: inputData.phone,
+      event_id: event.id,
+    });
   };
 
   return (
@@ -57,13 +73,31 @@ function BookEvent() {
         <div className="flex items-center justify-center h-screen">
           <Spinner />
         </div>
+      ) : error ? (
+        <div className="container py-10 md:py-24 mx-auto flex flex-wrap items-center">
+          <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+            <svg
+              className="w-12 h-12 text-gray-200"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 640 512"
+            >
+              <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+            </svg>
+          </div>
+          <div className="flex flex-col flex-1 items-center justify-center w-full md:w-1/2 text-red-400 gap-6 my-6">
+            <h1>error occured</h1>
+            <h1>Please comeback later</h1>
+          </div>
+        </div>
       ) : (
         <div className="container py-10 md:py-24 mx-auto flex flex-wrap items-center">
           <div className="sm:w-1/2  px-3 md:mb-10">
             <div className="rounded-lg h-64 overflow-hidden">
               <img
                 alt="content"
-                className="object-cover object-center h-full w-full"
+                className="object-fit object-center h-full w-full"
                 src={event.image_url}
                 //   src="https://dummyimage.com/1201x501"
               />
