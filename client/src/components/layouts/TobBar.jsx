299ef114d5e41c875faa00fd5374e623e92e9471
hamisirizwan <hamisirizwan5@gmail.com>
2023-03-16 12:37:30 +0300

    handled logout

--- client/src/components/layouts/TobBar.jsx
+++ client/src/components/layouts/TobBar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../store/slices/authSlice";
 
 function TobBar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout);
+    navigate("/sign-in");
+  };
   return (
-    <nav className="md:hidden bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="md:hidden bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com/" className="flex items-center">
           <img
@@ -10,14 +19,15 @@ function TobBar() {
             className="h-6 mr-3 sm:h-9"
             alt="Flowbite Logo"
           />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-xl font-semibold whitespace-nowrap ">
             E-TIKETI
           </span>
         </a>
         <div className="flex md:order-2">
           <a
             href="#"
-            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+            onClick={handleLogout}
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 "
           >
             <svg
               fill="#000000"
